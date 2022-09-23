ee3f87f8f45fc48c6662b225b1f21e8e24f80da5
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-23 11:42:52 +0000

    23-09-2022 11:42 Fase 2 do programa

--- programa5.py
+++ programa5.py
@@ -1,14 +1,22 @@
 def divisores(num):
+    zeros = 0
+    for n in range(1, num + 1):
+        if num % n == 0:
+            zeros += 1  # zeros = zeros + 1
+    return zeros
 
-
-    return 1
-
+def primo(num):
+    return num % 2 != 0
 
 if __name__ == '__main__':
     while True:
         numero = int(input('Digite o primeiro número: '))
 
-        print(f'O número {numero} tem divisores {divisores(numero)}')
+        if primo(numero):
+            print(f'O número {numero} tem {divisores(numero)} divisores e é primo.')
+        else:
+            print(f'O número {numero} tem {divisores(numero)} divisores e não é primo.')
+        #print(f'O número {numero} tem {divisores(numero)} divisores.')
 
         continuar = input('Repetir [s | n]? ')
         if continuar == 'n':
