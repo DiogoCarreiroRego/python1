7f9843282c3f66d611b5bb1ed7cf2119137bad6a
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-23 09:42:50 +0000

    23-09-2022 09:42

--- programa2.py
+++ programa2.py
@@ -2,7 +2,29 @@
 ESTE PROGRAMA DÁ A "LETRA" DA NOTA
 """
 
+def notas(nt):
+    nota = 'Nota Inválida'
+    if nt >= 90 <= 100:
+        nota = 'A'
+    elif nt >= 89 <= 70:
+        print('B')
+    elif nt >= 69 <= 50:
+        print('C')
+    elif nt >= 49 <= 30:
+        print('D')
+    elif nt >= 29 <= 10:
+        print('E')
+    elif nt >= 9 <= 0:
+        print('F')
 
+    return nota
+
+
+if __name__ == '__main__':
+    nota = int(input('Qual foi a sua nota? '))
+
+    print(f'Tem um = {notas(nota)}')
+"""
 def notas(nt):
     nota = 'Nota Inválida'
     if nt <= 100 <=90:
@@ -24,3 +46,4 @@ if __name__ == '__main__':
     nota = int(input('Qual foi a sua nota? '))
 
     print(f'Tem um = {notas(nota)}')
+"""
