0fad951311c4c3b9d9d2065123c2274295f61fa1
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-23 12:08:52 +0000

    23-09-2022 12:00 Pre-Terminado

---
+++ programa_ex2.py
@@ -0,0 +1,19 @@
+def par(numero):
+    return numero % 2 != 0
+
+if __name__ == '__main__':
+    while True:
+        num = int(input('Digite o número inicial: '))
+        quantos = int(input('Quantos? '))
+        primos = 0
+
+        for x in range(num1, num2 + 1):
+            if par(x):
+                primos += 1
+                print(x)
+        print(f'Tem {primos}')
+
+        continuar = input('Repetir [s | n]? ')
+        if continuar == 'n':
+            break
+    print(f'Adeus!')
