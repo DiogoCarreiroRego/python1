e76c4d6501a2bb2c622bc7d174d2bf251f9c27b3
DiogoCarreiroRego <diogocarreirorego@enta.pt>
2022-09-23 11:24:57 +0000

    23-09-2022 11:24 Fase 1 do programa

---
+++ programa5.py
@@ -0,0 +1,15 @@
+def divisores(numero):
+
+
+    return 1
+
+
+if __name__ == '__main__':
+    while True:
+        numero = int(input('Digite o primeiro número: '))
+
+        print(f'O número {numero} tem divisores {divisores(numero)}')
+
+        continuar = input('Repetir [s | n]? ')
+        if continuar == 'n':
+            break
